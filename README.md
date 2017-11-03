c92aa9ae7d891682ae9eab164028a15eeb705f45
Andrew T <16790483+ateesdalejr@users.noreply.github.com>
2017-11-03 16:39:15 -0700

    added timer and check for quit event.

---
+++ README.md
@@ -0,0 +1,8 @@
+# throwback-tanks
+
+Each tank has a cannon. These can be aimed vertically up and down. As well as having a power metric.
+Tanks fire bombs which have delayed explosive fuses. Players can get out of tanks and throw
+the explosives back. Explosives have a blast radius attack that instantly kills players
+in the radius and slightly damages tanks.
+
+There is a delay to getting out of your tank.
--- main.c
+++ main.c
@@ -1,27 +1,56 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
 
+const float FPS = 60;
+
 int main(int argc, char **argv){
     ALLEGRO_DISPLAY *display = NULL;
-
+    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+    ALLEGRO_TIMER *timer = NULL;
+    bool redraw = true, running = true;
     if(!al_init()) {
         fprintf(stderr, "failed to initialize allegro!\n");
         return -1;
     }
-
+    timer=al_create_timer(1.0/FPS);
+    if (!timer) {
+        fprintf(stderr, "failed to create timer!\n");
+        return -1;
+    }
     display = al_create_display(640, 480);
     if(!display) {
         fprintf(stderr, "failed to create display!\n");
+        al_destroy_timer(timer);
         return -1;
     }
-
+    event_queue = al_create_event_queue();
+    if (!event_queue) {
+        fprintf(stderr, "failed to create event queue!\n");
+        al_destroy_display(display);
+        al_destroy_timer(timer);
+        return -1;
+    }
+    al_register_event_source(event_queue,al_get_display_event_source(display));
+    al_register_event_source(event_queue,al_get_timer_event_source(timer));
     al_clear_to_color(al_map_rgb(0,0,0));
-
     al_flip_display();
-
-    al_rest(10.0);
-
+    al_start_timer(timer);
+    while (running) {
+        ALLEGRO_EVENT ev;
+        al_wait_for_event(event_queue,&ev);
+        if (ev.type == ALLEGRO_EVENT_TIMER) {
+            redraw=true;
+        } else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+            break;
+        }
+        if (redraw && al_is_event_queue_empty(event_queue)) {
+            redraw = false;
+            al_clear_to_color(al_map_rgb(0,0,0));
+            al_flip_display();
+        }
+    }
+    al_destroy_timer(timer);
     al_destroy_display(display);
-
+    al_destroy_event_queue(event_queue);
     return 0;
 }
